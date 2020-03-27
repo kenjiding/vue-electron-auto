6ba262366bfa3759797b5b7274fafaadd0293cc9
dinglun <dinglun@yy.com>
2020-03-27 16:05:27 +0800

    第一次提交

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # vue-electron
-electron + vue的项目，更具配置自动生产前端页面
+
+## Project setup
+```
+yarn install
+```
+
+### Compiles and hot-reloads for development
+```
+yarn serve
+```
+
+### Compiles and minifies for production
+```
+yarn build
+```
+
+### Lints and fixes files
+```
+yarn lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <button class="alt" @click="usingNode">渲染进程运行node接口</button>
+  </div>
+</template>
+
+<script>
+const fs = require('fs')
+const path = require('path')
+
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  methods: {
+    usingNode () {
+      const dirPath = path.resolve('./pages_config/')
+      var noneExistFileName = path.join(dirPath, ['async_create.', new Date() - 0, '.txt'].join(''))
+
+      if (!fs.existsSync(dirPath)) {
+        fs.mkdirSync(dirPath)
+      }
+
+      fs.writeFile(noneExistFileName, new Date(), function (err) {
+        if (err) throw err
+        alert(err + '创建了新的文件！')
+      })
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
